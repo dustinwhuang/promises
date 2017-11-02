bbb4c9f7e5ce221ed978e09b3d67f097e43c09ff
Hack Reactor Student <student@hackreactor.com>
2017-11-02 12:32:10 -0700

    Pass tests up to chaining

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,25 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(err, content.toString().split('\n')[0]);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request({url: url}, (err, res, body) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(err, res.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
